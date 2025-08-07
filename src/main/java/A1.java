67b325d38b174041786fa659cf950189bebbb106
Ahmad Khalid Saleh <aksaleh43@gmail.com>
2025-08-07 07:39:23 -0700

    Em02 commit

--- src/main/java/A1.java
+++ src/main/java/A1.java
@@ -3,5 +3,8 @@ public class A1 {
 
         System.out.println("Version 1");
         System.out.println("Version 2");
+
+        System.out.println(" This is the latest version");
+        System.out.println(" We will have the third commit");
     }
 }
